ec95f8a41d880b85cc098545cfb9c536178aff17
Simon Rochwerg <simon.rochwerg@lobstr.io>
2019-08-01 18:15:31 +0200

    [shadow_useragent] adds dependencies and change package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="shadow-useragent-simonlobstr",
+    name="shadow-useragent-lobstr",
     version="0.0.1",
     author="Simon Rochwerg",
     author_email="simon.rochwerg@lobstr.io",
@@ -13,6 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Lobstrio/shadow-useragent",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'pytz',
+        'coloredlogs'
+    ],
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
