c1db49d2f7b6f11aeefe347d10f98271182766d1
Simon Rochwerg <simon.rochwerg@lobstr.io>
2019-08-07 20:45:18 +0200

    [test.py] improve tests

--- shadow_useragent/test.py
+++ shadow_useragent/test.py
@@ -1,5 +1,14 @@
-from core import ShadowUserAgent
+from shadow_useragent import ShadowUserAgent
 
 shadow_useragent = ShadowUserAgent()
-for i in range(0, 10):
-    print(shadow_useragent.random)
+
+
+print(shadow_useragent.firefox)
+print(shadow_useragent.chrome)
+print(shadow_useragent.safari)
+print(shadow_useragent.edge)
+print(shadow_useragent.ie)
+print(shadow_useragent.android)
+print(shadow_useragent.ipad)
+print(shadow_useragent.random)
+print(shadow_useragent.random_nomobile)
