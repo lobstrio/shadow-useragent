4d654b1096ea71042bc0217fbe94c615e0a0a6ef
Simon Rochwerg <simon.rochwerg@lobstr.io>
2019-08-08 02:13:49 +0200

    [setup.py] release new version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*parts):
 
 setuptools.setup(
     name="shadow-useragent",
-    version="0.0.9",
+    version="0.0.10",
     author="Simon Rochwerg",
     author_email="simon.rochwerg@lobstr.io",
     description="Always get best user-agent",
